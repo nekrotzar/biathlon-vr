6466997ee486866d31f5c4b976140ce0039e9438
nekrotzar <luis.m.fonseca@hotmail.com>
2018-10-17 18:02:47 +0100

    Add HUD minimap

---
+++ Proj1/Assets/Scripts/MiniMapCamRot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamRot : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public GameObject player;
+    private Transform _mainCTtransf;
+
+	// Use this for initialization
+	void Start () {
+        _mainCTtransf = mainCamera.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //temp.position.Set(_mainCTtransf.position.x, this.transform.position.y, _mainCTtransf.position.z);
+        this.transform.LookAt(mainCamera.transform);
+    }
+}
