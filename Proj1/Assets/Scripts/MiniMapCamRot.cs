d945a8c32a32129df1ce6c05b7757212339f4c58
mrpatinhazz <jguilhermefgomes@gmail.com>
2018-10-20 18:01:41 +0100

    Collision Fixed
    Still work to do

--- Proj1/Assets/Scripts/MiniMapCamRot.cs
+++ Proj1/Assets/Scripts/MiniMapCamRot.cs
@@ -6,16 +6,9 @@ public class MiniMapCamRot : MonoBehaviour {
 
     public GameObject mainCamera;
     public GameObject player;
-    private Transform _mainCTtransf;
-
-	// Use this for initialization
-	void Start () {
-        _mainCTtransf = mainCamera.transform;
-	}
 	
 	// Update is called once per frame
 	void Update () {
-        //temp.position.Set(_mainCTtransf.position.x, this.transform.position.y, _mainCTtransf.position.z);
         this.transform.LookAt(mainCamera.transform);
     }
 }
