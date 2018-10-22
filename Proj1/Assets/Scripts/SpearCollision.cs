7b79e94b74b41841c379de7be9d852b60693bb57
Francisco Dourado <francisco.dourado@hotmail.com>
2018-10-22 22:09:23 +0100

    collisions

---
+++ Proj1/Assets/Scripts/SpearCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearCollision : MonoBehaviour {
+    public bool isAlive;
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        isAlive = true;
+        anim = GetComponent<Animator>();
+        //anim.SetBool("Dragon_isAlive", true);
+    }
+
+   
+
+    void OnTriggerEnter(Collider coll)
+    {/*
+        Debug.Log("olha eu a entrar");
+        isAlive = false;
+        */
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKey(KeyCode.O))
+            anim.SetBool("Dragon_isDead", false);
+        else if (Input.GetKey(KeyCode.P))
+            anim.SetBool("Dragon_isDead", true);
+        /*
+        if (isAlive)
+            anim.SetBool("Dragon_isDead", false);
+        else if (!isAlive)
+            anim.SetBool("Dragon_isDead", true);
+            */
+    }
+}
