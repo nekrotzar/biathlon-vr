10112e41289a51810c5605b9ca73b9654c4f0062
mrpatinhazz <jguilhermefgomes@gmail.com>
2018-10-23 11:48:19 +0100

    Lança mata o dragão

--- Proj1/Assets/Scripts/SpearCollision.cs
+++ Proj1/Assets/Scripts/SpearCollision.cs
@@ -3,36 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpearCollision : MonoBehaviour {
-    public bool isAlive;
-    public Animator anim;
 
-	// Use this for initialization
-	void Start () {
-        isAlive = true;
-        anim = GetComponent<Animator>();
-        //anim.SetBool("Dragon_isAlive", true);
-    }
-
-   
-
-    void OnTriggerEnter(Collider coll)
-    {/*
-        Debug.Log("olha eu a entrar");
-        isAlive = false;
-        */
-    }
-
-    // Update is called once per frame
-    void Update () {
-        if(Input.GetKey(KeyCode.O))
-            anim.SetBool("Dragon_isDead", false);
-        else if (Input.GetKey(KeyCode.P))
-            anim.SetBool("Dragon_isDead", true);
-        /*
-        if (isAlive)
-            anim.SetBool("Dragon_isDead", false);
-        else if (!isAlive)
-            anim.SetBool("Dragon_isDead", true);
-            */
+    void OnCollisionEnter(Collision col)
+    {
+		if (col.gameObject.tag == "Dragon") {
+			col.gameObject.SetActive (false);
+		}
     }
 }
