8fdf1fe2cc9eac24a53adc14282be2af78b04b78
mrpatinhazz <jguilhermefgomes@gmail.com>
2018-11-18 17:37:27 +0000

    Shooting a target changes his color
    Changes the albedo and specular to white on hit with raycast. The gun also is aligned with the crosshair

---
+++ VRBiathlon/Assets/VR_Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class VR_Shoot : MonoBehaviour {
+
+    public float range = 100f;
+    public Camera mainCamera;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+	}
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
+        {
+            if(hit.transform.tag == "Target")
+            {
+                Renderer rend = hit.transform.GetComponent<Renderer>();
+
+                rend.material.shader = Shader.Find("_Color");
+                rend.material.SetColor("_Color", Color.white);
+
+                rend.material.shader = Shader.Find("Specular");
+                rend.material.SetColor("_SpecColor", Color.white);
+            }
+        }
+
+
+    }
+}
