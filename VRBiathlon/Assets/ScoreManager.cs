c022b7e5aff0de7c5fc7b3d7fd2fcadd0d7f885b
nekrotzar <luis.m.fonseca@hotmail.com>
2018-11-19 22:49:43 +0000

    Add time manager script

---
+++ VRBiathlon/Assets/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ScoreManager : MonoBehaviour {
+
+    // String to display time
+    public Text timeText;
+
+    // The amount of time that has elapsed
+    private float elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+        elapsedTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var t = Time.time - elapsedTime;
+
+        timeText.text = FormatTime(t);
+	}
+
+    string FormatTime(float time){
+        TimeSpan t = TimeSpan.FromSeconds(time);
+
+        return string.Format("{0:D2}:{1:D2}:{2:D2}", t.Minutes, t.Seconds, (t.Milliseconds / 10));
+    }
+}
